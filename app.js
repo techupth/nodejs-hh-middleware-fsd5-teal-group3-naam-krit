d3914cb18f4cdf07e12009ee360cc12e222ef082
Krit <titatit.akkh@gmail.com>
2024-01-10 09:02:49 +0700

    feat:finished EX-Middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import assignmentRouter from "./apps/assignments.js";
+import logging from "./middlewares/logging.js";
 
 const app = express();
 const port = 4000;
+
+app.use(logging);
 
 app.use(bodyParser.json());
 app.use("/assignments", assignmentRouter);
